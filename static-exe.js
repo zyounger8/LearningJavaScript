e67b9b177221f143135d9687e006cd2276c47ea0
Zee Louise <zeelouise@zees-imac.myfiosgateway.com>
2022-09-12 23:53:31 -0400

    takethree

---
+++ static-exe.js
@@ -0,0 +1,36 @@
+// Static Methods and Properties
+// class Governor {
+//     constructor(name){
+//         this.name = name
+//         this.title = 'title'
+//     }
+// }
+
+//Inheritance
+class Person {
+  constructor(name1, age) {
+    this._name = name1;
+    this._age = age;
+    this._remainingYearsWorking = 25;
+  }
+
+  get name1() {
+    return this.name1;
+  }
+
+  get age() {
+    return this.age;
+  }
+
+  get remainingYearsWorking() {
+    return this._remainingYearsWorking;
+  }
+
+  countdownToRetire(yearsWorked) {
+    this.remainingYearsWorking -= yearsWorked;
+  }
+}
+
+class PostalWorker extends Person {
+  constructor(name1, age) {}
+}
