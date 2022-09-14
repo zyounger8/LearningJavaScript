d5e2224c95f04ae7f8f8e65e2ebea9c5c4e652a5
Zee Louise <zeelouise@zees-imac.myfiosgateway.com>
2022-09-13 20:22:19 -0400

    Wild Wild Life answered

--- alc-exe.js
+++ alc-exe.js
@@ -31,3 +31,27 @@ for (i = 1; i < 100; i++) {
     console.log(i);
   }
 }
+
+//////////////////
+// Wild Wild Life
+/////////////////
+
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
+const sharky = ["Sharky", "shark", 20, "Left Coast"];
+const plantee = ["Plantee", "plant", 5000, "Mordor"];
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
+const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"];
+
+// Number 1
+plantee[2] = 5001;
+console.log(plantee);
+// Number 2
+wolfy[3] = "Gotham City";
+console.log(wolfy);
+// Number 3
+dart.push("Hawkins");
+console.log(dart);
+// Number 4
+wolfy.shift();
+wolfy.unshift("Gameboy");
+console.log(wolfy);
