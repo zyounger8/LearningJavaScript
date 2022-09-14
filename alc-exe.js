8f258a569defbff384d1aa07df92bd9c48d309ba
Zee Louise <zeelouise@zees-imac.myfiosgateway.com>
2022-09-13 20:03:55 -0400

    Fizz Buzz answered

--- alc-exe.js
+++ alc-exe.js
@@ -15,3 +15,19 @@ for (i = 0; i <= 200; i++) {
     console.log(i);
   }
 }
+
+//////////////
+// Fizz Buzz
+/////////////
+
+for (i = 1; i < 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
